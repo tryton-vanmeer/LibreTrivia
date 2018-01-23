d9ca942990cb95fe215fd0db9b3fe511cb9d2ab5
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-01-23 02:13:42 -0500

    Added trivia question type enum, and string values for enum

---
+++ app/src/main/java/io/github/trytonvanmeer/libretrivia/OpenTrivia/TriviaType.java
@@ -0,0 +1,21 @@
+package io.github.trytonvanmeer.libretrivia.OpenTrivia;
+
+import android.content.Context;
+
+import io.github.trytonvanmeer.libretrivia.R;
+
+public enum TriviaType {
+    MULTIPLE(R.string.question_type_multiple),
+    BOOLEAN(R.string.question_type_boolean);
+
+    // The display name of the category
+    private int displayName;
+
+    TriviaType(int displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName(Context context) {
+        return context.getResources().getString(this.displayName);
+    }
+}
