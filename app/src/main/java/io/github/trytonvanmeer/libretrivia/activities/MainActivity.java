0cac7151bb8f6b84694275a57119368438288d14
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-01-24 00:37:29 -0500

    Created MainActivity

--- app/src/main/java/io/github/trytonvanmeer/libretrivia/activities/MainActivity.java
+++ app/src/main/java/io/github/trytonvanmeer/libretrivia/activities/MainActivity.java
@@ -1,14 +1,27 @@
 package io.github.trytonvanmeer.libretrivia.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.trytonvanmeer.libretrivia.R;
 
 public class MainActivity extends BaseActivity {
+    @BindView(R.id.button_play) Button buttonPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Start Game
+            }
+        });
     }
 }
