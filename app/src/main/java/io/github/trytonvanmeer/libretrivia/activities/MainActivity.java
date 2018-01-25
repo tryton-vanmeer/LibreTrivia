1d810827f0e0c1354cbaa103989e21728ad511f7
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-01-24 20:45:09 -0500

    Added ability to select game options from main menu

--- app/src/main/java/io/github/trytonvanmeer/libretrivia/activities/MainActivity.java
+++ app/src/main/java/io/github/trytonvanmeer/libretrivia/activities/MainActivity.java
@@ -2,14 +2,20 @@ package io.github.trytonvanmeer.libretrivia.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.trytonvanmeer.libretrivia.R;
+import io.github.trytonvanmeer.libretrivia.trivia.TriviaCategory;
+import io.github.trytonvanmeer.libretrivia.trivia.TriviaDifficulty;
 
 public class MainActivity extends BaseActivity {
     @BindView(R.id.button_play) Button buttonPlay;
+    @BindView(R.id.spinner_category) Spinner spinnerCategory;
+    @BindView(R.id.spinner_difficulty) Spinner spinnerDifficulty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +29,13 @@ public class MainActivity extends BaseActivity {
                 // TODO: Start Game
             }
         });
+
+        spinnerCategory.setAdapter(
+                new ArrayAdapter<>(
+                        this, android.R.layout.simple_list_item_1, TriviaCategory.values()));
+
+        spinnerDifficulty.setAdapter(
+                new ArrayAdapter<>(
+                        this, android.R.layout.simple_list_item_1, TriviaDifficulty.values()));
     }
 }
