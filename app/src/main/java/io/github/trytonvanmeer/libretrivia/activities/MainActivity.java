9d2e11a20c60e8bff1ad56e7e0373979abd53529
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-01-24 21:09:55 -0500

    Play Button now creates TriviaQuery based on user input

--- app/src/main/java/io/github/trytonvanmeer/libretrivia/activities/MainActivity.java
+++ app/src/main/java/io/github/trytonvanmeer/libretrivia/activities/MainActivity.java
@@ -1,21 +1,28 @@
 package io.github.trytonvanmeer.libretrivia.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+
+import com.shawnlin.numberpicker.NumberPicker;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.trytonvanmeer.libretrivia.R;
 import io.github.trytonvanmeer.libretrivia.trivia.TriviaCategory;
 import io.github.trytonvanmeer.libretrivia.trivia.TriviaDifficulty;
+import io.github.trytonvanmeer.libretrivia.trivia.TriviaQuery;
 
 public class MainActivity extends BaseActivity {
+    private static final String TAG = "MainActivity";
+
     @BindView(R.id.button_play) Button buttonPlay;
     @BindView(R.id.spinner_category) Spinner spinnerCategory;
     @BindView(R.id.spinner_difficulty) Spinner spinnerDifficulty;
+    @BindView(R.id.number_picker) NumberPicker numberPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,14 @@ public class MainActivity extends BaseActivity {
         buttonPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: Start Game
+                int amount = numberPicker.getValue();
+                TriviaCategory category = (TriviaCategory) spinnerCategory.getSelectedItem();
+                TriviaDifficulty difficulty = (TriviaDifficulty) spinnerDifficulty.getSelectedItem();
+
+                TriviaQuery query = new TriviaQuery.Builder(amount)
+                        .category(category)
+                        .difficulty(difficulty)
+                        .build();
             }
         });
 
