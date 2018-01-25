76beb2aa274e03cced32dd95ee9e59b0bf96dcf7
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-01-25 08:52:46 -0500

    Cleaned up unused imports.

--- app/src/main/java/io/github/trytonvanmeer/libretrivia/trivia/TriviaQuestion.java
+++ app/src/main/java/io/github/trytonvanmeer/libretrivia/trivia/TriviaQuestion.java
@@ -1,7 +1,5 @@
 package io.github.trytonvanmeer.libretrivia.trivia;
 
-import com.google.gson.JsonObject;
-
 public abstract class TriviaQuestion<T> {
     private TriviaCategory category;
     private TriviaDifficulty difficulty;
