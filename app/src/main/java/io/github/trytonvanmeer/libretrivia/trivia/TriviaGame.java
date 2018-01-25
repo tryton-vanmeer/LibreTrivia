3a91055bfcf9f23b5dd91288d68c2f39d8e04eb9
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-01-25 04:04:11 -0500

    Added TriviaGame. Acts as game state object

---
+++ app/src/main/java/io/github/trytonvanmeer/libretrivia/trivia/TriviaGame.java
@@ -0,0 +1,29 @@
+package io.github.trytonvanmeer.libretrivia.trivia;
+
+import java.util.List;
+
+public class TriviaGame {
+    private static final String TAG = "TriviaGame";
+
+    private int currentQuestion;
+    private boolean[] questionsAnsweredStatus;
+    private List<TriviaQuestion> questions;
+
+    public TriviaGame(List<TriviaQuestion> questions) {
+        this.currentQuestion = 1;
+        this.questions = questions;
+        this.questionsAnsweredStatus = new boolean[questions.size()];
+    }
+
+    public TriviaQuestion getCurrentQuestion() {
+        return this.questions.get(currentQuestion);
+    }
+
+    public int getCurrentQuestionCount() {
+        return this.currentQuestion;
+    }
+
+    public int getQuestionsCount() {
+        return this.questions.size();
+    }
+}
